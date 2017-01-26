b1239a7a06225576f596a1ec4e0058c9a98f0977
faspaydeveloper <faspaydeveloper@gmail.com>
2017-01-26 11:25:31 +0700

    Update SignResponse.php

--- SignResponse.php
+++ SignResponse.php
@@ -1,40 +1,102 @@
 <html>
 <head>
-    <link rel= stylesheet href= "bootstrap/css/bootstrap.css">
-    <script src="jquery-2.2.1.js"></script>
-    <script type= "text/javascript" src="bootstrap/js/bootstrap.js"></script>
-    <link rel= stylesheet href= "bootstrap/css/bootstrap.min.css">
+<TITLE>Faspay - Generate Signature Test</TITLE>
+<script src="sha1.js"></script>
+<script language="JavaScript">
+	function generateHash(form) {
+	  var actionurl = form.actionurl.value;
+	  form.action = actionurl;
+
+	  if(form.GEN_HASH.value == "Yes"){
+		var str = "##" + form.MERCHANTID.value.toUpperCase() + "##" + form.TXN_PASSWORD.value.toUpperCase() + "##" + form.MERCHANT_TRANID.value.toUpperCase() + "##" + form.AMOUNT.value.toUpperCase() + "##0##";
+		form.SIGNATURE.value = hex_sha1(str);
+		alert (str);
+	  }
+	}
+	function generateTranID(form){
+		var date = new Date();
+
+		var yy = date.getFullYear();
+		var m = date.getMonth() + 1;
+		var d  = date.getDate();
+
+		var day = (d < 10) ? '0' + d : d;
+		var month = (m < 10) ? '0' + m : m;
+		var year = (yy < 1000) ? yy + 2000 : yy;
+
+		var hh = date.getHours();
+		var mm = date.getMinutes();
+		var ss = date.getSeconds();
+
+		var hour = (hh<10)?'0'+hh:hh;
+		var min = (mm<10)?'0'+mm:mm;
+		var sec = (ss<10)?'0'+ss:ss;
+
+		form.merchant_tranid.value = ''+year+month+day+'_'+hour+min+sec;
+	}
+</script>
 </head>
-</head>
-<body>
-	<div class="container">
-		<div class="jumbotron">
-		<h1>Signature Request-Response</h1>
-		</div>
 
-		<form action="" method="post">
-			<p>Merchant ID <input type="text" name="merchant_id" class="form-control" value="TEST01"></p>
-			<p>Password <input type="password" name="sTxnPassword" class="form-control" value="059B8125F"></p>
-			<p>Merchant TranID <input type="text" name="merchant_tranid" class="form-control"  value="20061123_003"></p>
-			<p>Amount <input type="text" name="amount" class="form-control" value="100.00"></p>
-			<p>Transaction ID <input type="text" name="transaction_id" class="form-control" value="50126"></p>
-			<button type="submit" name="submit" class="btn btn-success">Submit</button></a>
-		</form>
+<body >
+<p align="left"><b>This is the payment test page for integrating to Generate Signature:</b></p>
 
-    	<?php
-    		if(isset($_POST['submit'])){
-    			$merchant_id = $_POST['merchant_id'];
-	            $sTxnPassword = $_POST['sTxnPassword'];
-	            $merchant_tranid = $_POST['merchant_tranid'];
-	            $amount = $_POST['amount'];
-	            $transaction_id = $_POST['transaction_id'];
-	            $hash='##'.strtoupper($merchant_id).'##'.strtoupper($sTxnPassword).'##'.$merchant_tranid.'##'.$amount.'##'.$transaction_id.'##';
+<form ACTION="" method="POST" name="theForm" content-type="application/x-www-form-urlencoded">
+  <div>
+    <table border="1" cellspacing="1" cellpadding="2" style="width: 100%">
+      <tr bgcolor="#FFFF99"> 
+        <td width="301"><b>Remarks</b></td>
+        <td width="320"><b>Parameter Name</b></td>
+        <td width="333"><b>Value</b></td>
+      </tr>
+      <tr>
+        <td>Merchant Id assigned by Faspay<font color="red"><b>**</b></font></td>
+        <td>MERCHANTID</td>
+        <td> <input name="merchant_id" size="30" value="TEST01" required> </td>
+      </tr>
+      <tr>
+        <td>Merchant Reference no (must be unique, max 100 characaters)<font color="red"><b>**</b></font></td>
+        <td>MERCHANT_TRANID &nbsp;&nbsp <input type="button" value="Generate ID" onClick="generateTranID(document.theForm);"/> </td>
+        <td> <input name="merchant_tranid" size="30" value="" required> </td>
+      </tr>
+      <tr> 
+        <td>Transaction amount (2 decimal, e.g. 54.20)<font color="red"><b>**</b></font></td>
+        <td>AMOUNT</td>
+        <td> <input name="amount" size="20" value="100.00" required> </td>
+      </tr>
+      <tr> 
+        <td>Faspay assigned transaction id during authorization request</td>
+        <td>TRANSACTION</td>
+        <td> <input name="transaction_id" size="20" value="50126" required> </td>
+      </tr>
+      <tr> 
+        <td colspan=2> <font color="#3300FF">Please enter your merchant transaction password </font>:
+        <br>This is only required for testing with this page to generate your transaction signature. 
+        <br>For production, you should never submit your transaction password.
+        </td>
+        <td> <input name="sTxnPassword" size="50" value="059B8125F" required> </td>
+      </tr>
+      <tr> 
+        <td colspan="4" align="center"> 
+        <button type="submit" name="submit" class="btn btn-success">Submit</button></a></td>
+      </tr>
+    </table>    
 
-            	$hes = sha1($hash);
-            	echo "<div class='alert alert-success' style='text-transform:uppercase;' >".strtoupper($hes)."</div>";
-			}    			
-    	?>
-	</div>
+  </div>
+</form>
+<?php
+  if(isset($_POST['submit'])){
+    $merchant_id = $_POST['merchant_id'];
+        $sTxnPassword = $_POST['sTxnPassword'];
+        $merchant_tranid = $_POST['merchant_tranid'];
+        $amount = $_POST['amount'];
+        $transaction_id = $_POST['transaction_id'];
+        $hash='##'.strtoupper($merchant_id).'##'.strtoupper($sTxnPassword).'##'.strtoupper($merchant_tranid).'##'.strtoupper($amount).'##'.strtoupper($transaction_id).'##';
+
+        $hes = sha1($hash);
+        echo strtoupper($hes);
+}
+?>
 
 </body>
+</html>
 </html>
