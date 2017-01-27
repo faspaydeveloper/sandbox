ccfab1781e581f974e7fed45bb6dc291af289630
faspaydeveloper <faspaydeveloper@gmail.com>
2017-01-27 11:05:42 +0700

    Update gensignfpnativeres.php

--- gensignfpnativeres.php
+++ gensignfpnativeres.php
@@ -5,7 +5,7 @@
 	$password = $_POST['password'];
 	$order_id = $_POST['order_id'];
 
-	$signature = 'bot'.$user_id.$password.$order_id;
+	$signature = $user_id.$password.$order_id;
     $hash = sha1(md5($signature);
 
     echo $hash;
